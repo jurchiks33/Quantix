cdce97f0b690238bdef7599c3404949eb76cd2b1
jurchiks33 <jurisslisans@gmail.com>
2024-05-13 16:10:35 +0200

    creating navbar

--- quantix.py
+++ quantix.py
@@ -1,28 +1,31 @@
 import tkinter as tk
+from tkinter import ttk
+
+def load_page(page):
+    #Placeholder to load different pages.
+    print(f'Loading page {page}')
 
 def main():
     #Main application window.
     root = tk.Tk()
     root.title("Quantix - analytical platform")
-
-    #Background color.
     root.configure(bg='#1a1a1a')
 
-    #Calculate dimensions for the window to be 70% of the screen size.
+    #dimensions for the window 70% of the screen size, center, position.
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     width = int(screen_width * 0.7)
     height = int(screen_height * 0.7)
-
-    #Center the window on the screen.
     x_offset = int((screen_width - width) / 2)
     y_offset = int((screen_height - height) / 2) 
-
-    #Set the initial position and the size of the window.
     root.geometry(f'{width}x{height}+{x_offset}+{y_offset}')
-    
+
+    #Navigation bar setup
+    navbar = ttk.Frame(root)
+    navbar.pack(side="top", fill="x")
 
     #Start Tkinetr event loop.
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
