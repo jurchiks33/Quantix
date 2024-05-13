2879b1eb42997d8711e609ef1e8d8fbea36915d2
jurchiks33 <jurisslisans@gmail.com>
2024-05-13 16:02:33 +0200

    small syntax fixes

--- quantix.py
+++ quantix.py
@@ -2,11 +2,11 @@ import tkinter as tk
 
 def main():
     #Main application window.
-    root = tk.TK()
+    root = tk.Tk()
     root.title("Quantix - analytical platform")
 
     #Background color.
-    root.configure(bg='1a1a1a')
+    root.configure(bg='#1a1a1a')
 
     #Calculate dimensions for the window to be 70% of the screen size.
     screen_width = root.winfo_screenwidth()
@@ -20,4 +20,9 @@ def main():
 
     #Set the initial position and the size of the window.
     root.geometry(f'{width}x{height}+{x_offset}+{y_offset}')
-       
+    
+
+    #Start Tkinetr event loop.
+
+if __name__ == "__main__":
+    main()
